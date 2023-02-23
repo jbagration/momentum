60efb03b0f4ba67126453518aaf54428dc3746ae
jbagration <blissdery@gmail.com>
2023-02-23 17:08:58 +0500

    new file:   assets/favicon.ico 	new file:   assets/fonts/Arial-MT.woff 	new file:   assets/fonts/owfont-regular.eot 	new file:   assets/fonts/owfont-regular.otf 	new file:   assets/fonts/owfont-regular.svg 	new file:   assets/fonts/owfont-regular.ttf 	new file:   assets/fonts/owfont-regular.woff 	new file:   assets/img/bg.jpg 	new file:   assets/sounds/I Was Made For Lovin You.mp3 	new file:   assets/sounds/Paint It Black.mp3 	new file:   assets/sounds/Pink Venom.mp3 	new file:   assets/sounds/The Hills.mp3 	new file:   assets/svg/pause.svg 	new file:   assets/svg/play-next.svg 	new file:   assets/svg/play-prev.svg 	new file:   assets/svg/play.svg 	new file:   assets/svg/reload.svg 	new file:   assets/svg/settings-svgrepo-com.svg 	new file:   assets/svg/slider-next.svg 	new file:   assets/svg/slider-prev.svg 	new file:   css/owfont-regular.css 	new file:   js/playList.js

---
+++ js/lang.js
@@ -0,0 +1,22 @@
+const langArr = {
+    'title' : {
+        'en' : 'momentum',
+        'ru' : 'моментум'
+    },
+    'greeting' : {
+        'en' : 'Good afternoon',
+        'ru' : 'Добрый день'
+    },
+    'title' : {
+        'en' : '',
+        'ru' : ''
+    },
+    'title' : {
+        'en' : '',
+        'ru' : ''
+    },
+    'title' : {
+        'en' : '',
+        'ru' : ''
+    },
+}
---
+++ js/playList.js
@@ -0,0 +1,25 @@
+const playList = [
+  {      
+    title: 'Pink Venom',
+    src: '/assets/sounds/Pink Venom.mp3',
+    duration: '03:07'
+  },  
+  {      
+    title: 'The Hills',
+    src: '/assets/sounds/The Hills.mp3',
+    duration: '04:02'
+  },  
+    {      
+      title: 'Paint It Black',
+      src: '/assets/sounds/Paint It Black.mp3',
+      duration: '03:25'
+    },  
+
+
+    {      
+      title: 'I Was Made For Lovin You',
+      src: '/assets/sounds/I Was Made For Lovin You.mp3',
+      duration: '04:42'
+    }
+  ]
+  export default playList;
